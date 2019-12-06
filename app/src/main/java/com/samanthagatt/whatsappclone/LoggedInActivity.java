5db9021ac9c483d36f0eae21ac925abe9b3507e9
Samantha Gatt <smfgatt@gmail.com>
2019-12-06 08:35:52 -0500

    Implements ability to logout

--- app/src/main/java/com/samanthagatt/whatsappclone/LoggedInActivity.java
+++ app/src/main/java/com/samanthagatt/whatsappclone/LoggedInActivity.java
@@ -2,7 +2,13 @@ package com.samanthagatt.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoggedInActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class LoggedInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        Button mLogoutButton = findViewById(R.id.logoutButton);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                presentLoginActivity();
+            }
+        });
+    }
+
+    private void presentLoginActivity() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            Intent mIntent = new Intent(getApplicationContext(), MainActivity.class);
+            mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            finish();
+            return;
+        }
     }
 }
