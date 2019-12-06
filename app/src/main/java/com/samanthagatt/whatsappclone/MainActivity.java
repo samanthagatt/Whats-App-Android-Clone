81bd6fb6a9693df77a281a8964012aebecf5be5d
Samantha Gatt <smfgatt@gmail.com>
2019-12-06 13:30:18 -0500

    Renames login and main activities

--- app/src/main/java/com/samanthagatt/whatsappclone/MainActivity.java
+++ app/src/main/java/com/samanthagatt/whatsappclone/MainActivity.java
@@ -1,117 +1,59 @@
 package com.samanthagatt.whatsappclone;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthProvider;
-
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText mPhoneNumberEditText, mCodeEditText;
-    private String mVerificationCode;
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseApp.initializeApp(this);
-
-        presentLoggedInActivityIfAble();
-
-        mPhoneNumberEditText = findViewById(R.id.phoneNumberEditText);
-        mCodeEditText = findViewById(R.id.codeEditText);
-        final Button mLoginButton = findViewById(R.id.loginButton);
-
-        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-            @Override
-            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                loginWithPhoneAuthCred(phoneAuthCredential);
-            }
-
-            @Override
-            public void onVerificationFailed(@NonNull FirebaseException e) {
-                // TODO: Handle exception
-                Log.println(Log.DEBUG, "exception thrown",e.toString());
-            }
-
-            @Override
-            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                super.onCodeSent(s, forceResendingToken);
-
-                mLoginButton.setText("Verify Phone Number");
-
-                mVerificationCode = s;
-            }
-        };
-
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
+        Button mLogoutButton = findViewById(R.id.logoutButton);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                attemptLogin();
+                FirebaseAuth.getInstance().signOut();
+                presentLoginActivity();
             }
         });
+
+        Button mFindUserButton = findViewById(R.id.findUserButton);
+        mFindUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FindUserActivity.class));
+            }
+        });
+
+        getPermissions();
     }
 
-    private void attemptLogin() {
-        if (mVerificationCode != null) {
-            // Verification code already sent
-            PhoneAuthCredential mCred = PhoneAuthProvider.getInstance().getCredential(mVerificationCode, mCodeEditText.getText().toString());
-            loginWithPhoneAuthCred(mCred);
-        } else {
-            // Need to send verification code
-            PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                    mPhoneNumberEditText.getText().toString(),
-                    60,
-                    TimeUnit.SECONDS,
-                    this,
-                    mCallbacks
-            );
+    private void presentLoginActivity() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            Intent mIntent = new Intent(getApplicationContext(), LoginActivity.class);
+            mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            finish();
+            return;
         }
     }
 
-    private void loginWithPhoneAuthCred(PhoneAuthCredential phoneAuthCredential) {
-        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(
-                this,
-                new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            presentLoggedInActivityIfAble();
-                        } else {
-                            // TODO: Handle exception
-                            Log.println(Log.DEBUG, "Task not successful", task.getException().toString());
-                        }
-                    }
-                }
-        );
-    }
-
-    private void presentLoggedInActivityIfAble() {
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            startActivity(new Intent(getApplicationContext(), LoggedInActivity.class));
-            // Makes sure user cannot come back to page
-            finish();
-            return;
+    private void getPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[] {
+                    Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS },
+                    1);
         }
     }
 }
